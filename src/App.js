b9373bea4096f10e871890d3954bb81e154df5ea
Alessandra Perricone <alessandraperricone3@gmail.com>
2022-02-08 15:38:18 +0100

    feat: struttura HTML

--- src/App.js
+++ src/App.js
@@ -9,17 +9,25 @@ function App() {
       </div>
       <div className="flexItem-chatt">
         <div className="conversations">
+          <div className="userProfile">
+            <div className="profile"></div>
+            <h4>Pippo</h4>
+          </div>
           <h1>Conversazioni</h1>
         </div>
         <div className="messages">
           <h2>Nome </h2>
           <ul>
-            <li >
+            <li>
               <h3>Nome utente</h3>
               <h5>Giorno e Data</h5>
               <p>Text</p>
             </li>
           </ul>
+          <div className="input">
+            <p>Scrivi qui il testo</p>
+            <input type="textarea"/>
+          </div>
            </div>
       </div>
     </div>
