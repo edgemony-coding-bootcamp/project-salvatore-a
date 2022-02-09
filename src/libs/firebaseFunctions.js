3168b5dd05a35cd6d1d434225e3a060f6d5aa74c
Giorgio <giorgio.collova@gmail.com>
2022-02-09 12:28:13 +0100

    feat:add new functions relatives to groupscollection

--- src/libs/firebaseFunctions.js
+++ src/libs/firebaseFunctions.js
@@ -1,47 +1,95 @@
-import app from './firebase.config';
+import app from "./firebase.config";
 
-import {getFirestore, doc, setDoc, updateDoc, collection,query,/* where, */onSnapshot } from 'firebase/firestore';
-
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  updateDoc,
+  collection,
+  query,
+  /* where, */ onSnapshot,
+} from "firebase/firestore";
 
 const db = getFirestore(app);
-
+//-------------------------------------------------------------GET USERS----------------------------------------------------------------------------//
 async function getUsers() {
-    const q =  query(collection(db, "users"),/* where("logged","==", true) */);
-const unsubscribe = onSnapshot(q, (querySnapshot) => {
   const users = [];
-   querySnapshot.forEach((doc) => {
+  const q = query(collection(db, "users") /* where("logged","==", true) */);
+  onSnapshot(q, (querySnapshot) => {
+    querySnapshot.forEach((doc) => {
       users.push(doc.data());
+    });
   });
-  console.log( users);
-});
-
+  console.log(users);
+  return users
 }
+//-------------------------------------------------------------/GET USERS----------------------------------------------------------------------------//
 
-async function addUser(user,uid){
-    try {
-        const docRef = await setDoc(doc(db, 'users',uid), {
-            name: user.name,
-            lastname:user.lastname,
-            email: user.email,            
-            logged:false,
-            id:uid
-        });
-        return docRef
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+//-------------------------------------------------------------ADD USERS-----------------------------------------------------------------------------//
+async function addUser(user, uid) {
+  try {
+    const docRef = await setDoc(doc(db, "users", uid), {
+      name: user.name,
+      lastname: user.lastname,
+      email: user.email,
+      logged: false,
+      id: uid,
+    });
+    return docRef;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
+//-------------------------------------------------------------/ADD USERS----------------------------------------------------------------------------//
 
-async function patchUser(uid){
-    await updateDoc(doc(db, 'users', uid), {
-        
-        logged:true
-      });
+//-------------------------------------------------------------PATCH USERS---------------------------------------------------------------------------//
+async function patchUser(uid) {
+  await updateDoc(doc(db, "users", uid), {
+    logged: true,
+  });
 }
+//-------------------------------------------------------------/PATCH USERS--------------------------------------------------------------------------//
 
-
-export {
-    getUsers,
-    addUser,
-    patchUser
+//-------------------------------------------------------------GET GROUPS----------------------------------------------------------------------------//
+async function getGroups() {
+  const groups = [];
+  const q = query(collection(db, "groups") /* where("logged","==", true) */);
+  onSnapshot(q, (querySnapshot) => {
+    
+    querySnapshot.forEach((doc) => {
+      groups.push({name:doc.id,messages:doc.data().messages});
+    });
+    
+    
+  });
+  
+ 
+  
 }
+//-------------------------------------------------------------/GET GROUPS---------------------------------------------------------------------------//
+
+//-------------------------------------------------------------ADD GROUPS----------------------------------------------------------------------------//
+async function addGroup(name) {
+  try {
+    const docRef = await setDoc(doc(db, "groups", name), {
+      messages:[]
+    });
+    return docRef;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+//-------------------------------------------------------------ADD GROUPS----------------------------------------------------------------------------//
+
+//-------------------------------------------------------------PATCH USERS---------------------------------------------------------------------------//
+async function patchGroups(name,updatedMessages) {
+  await updateDoc(doc(db, "groups", name), {
+    //mandare un array contenente tutti i messaggi precedenti + il nuovo messaggio
+    messages: updatedMessages
+    
+  });
+}
+//-------------------------------------------------------------/PATCH USERS--------------------------------------------------------------------------//
+
+
+export { getUsers, addUser, patchUser,getGroups,addGroup,patchGroups };
