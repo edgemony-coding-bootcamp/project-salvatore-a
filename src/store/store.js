b60ea5b1794eb3cb2fedf39df746c22367b1cc0a
Giorgio <giorgio.collova@gmail.com>
2022-02-16 16:55:24 +0100

    fixing style normalization

--- src/store/store.js
+++ src/store/store.js
@@ -17,6 +17,6 @@ const initStore = {
     }
 }
 
-const store = createStore(myReducer,initStore,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(myReducer,initStore,compose(applyMiddleware(thunk)))
 
 export default store
