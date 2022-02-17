b2cdff01f7ca2b1d61ce81cd03de4c9455466142
Antonio Tutone <tutoneantonio@gmail.com>
2022-02-17 18:14:35 +0100

    fix: build error

--- src/Components/ProfileCard/index.jsx
+++ src/Components/ProfileCard/index.jsx
@@ -16,7 +16,7 @@ const ProfileCard = (props) => {
                 </button>
             </div>
             <img src={props.user.photo} alt={props.user.name}></img>
-            <h2 className={style.name}>{`${props.user.name}` + ` ` + `${props.user.lastname}`}</h2>
+            <h2 className={style.name}>{props.user.name+ ` ` + props.user.lastname}</h2>
             <p>Indirizzo email</p>
             <p className={style.email}>{props.user.email}</p>
             <Link to="/edit_profile">
