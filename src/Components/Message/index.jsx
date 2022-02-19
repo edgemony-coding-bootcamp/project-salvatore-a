89a5bc199ff8387dfd717ffecd02020462360069
Giorgio <giorgio.collova@gmail.com>
2022-02-19 11:14:07 +0100

    feat:update messages logic and style

--- src/Components/Message/index.jsx
+++ src/Components/Message/index.jsx
@@ -25,6 +25,8 @@ function toDateTime(secs) {
 export const Message = (props) => {
   const message = props.data;
   const date = message.date || { seconds: 1644405843 };
+  const [updateMessage,setUpdateMessage] = useState({message:message,status:false})
+  
   const [user, setUser] = useState({
     name: "",
     lastname: "",
@@ -54,6 +56,16 @@ export const Message = (props) => {
     );
     patchGroups("messages", message.message_group, updatedMessages);
   }
+  function handleUpdateMessage(e) {
+    e.preventDefault();
+    let updatedMessages = props.messages.map((x) => { 
+      if(x.message_id === message.message_id){return updateMessage.message} 
+      return x
+    });
+    
+    patchGroups("messages", message.message_group, updatedMessages);
+    setUpdateMessage({...updateMessage,status:!updateMessage.status});
+  }
 
   return (
     <>
@@ -65,6 +77,18 @@ export const Message = (props) => {
         onMouseOver={() => setLilModalDisplay(true)}
         className={style.message}
       >
+        
+          <form className={style.updateMessageForm} onSubmit={(e)=> handleUpdateMessage(e)}>
+          
+            <input 
+              value={updateMessage.message.text} 
+              onChange={(e)=> setUpdateMessage({...updateMessage,message:{...message,text:e.target.value}})} 
+              className={`${updateMessage.status ? style.updateMessage : style.hiddenUpdateMessage}`} 
+              type="text" 
+            />
+            
+          </form>
+        
         <div className={style.author}>
           <img
             src={user.photo}
@@ -76,12 +100,12 @@ export const Message = (props) => {
               <h3>{user.name + " " + user.lastname}</h3>
               <ShowTime date={toDateTime(date.seconds)} />
             </div>
-            <p>{message.text}</p>
+            {updateMessage.status === false && <p>{message.text}</p>}
           </div>
           {lilModalDisplay && message.author === currentUser.id && (
             <div className={style.lilModal}>
               <button onClick={() => handleDeleteMessage()}>❌</button>
-              <button>✍️</button>
+              <button onClick={()=> setUpdateMessage({...updateMessage,status:!updateMessage.status})}>✍️</button>
             </div>
           )}
         </div>
