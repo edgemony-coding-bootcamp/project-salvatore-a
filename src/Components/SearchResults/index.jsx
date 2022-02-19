3fb0be415451a60ae2dfe3a7e38730bc96881270
silvia.hth <silvia.hth@gmail.com>
2022-02-19 12:24:13 +0100

    fix: style mobile

--- src/Components/SearchResults/index.jsx
+++ src/Components/SearchResults/index.jsx
@@ -9,23 +9,16 @@ export const SearchResults = (props) => {
 
   return (
         <div className={style.searchResults}>
-           
-            {/* <Link className= {style.results} onClick={()=> props.setHidden(true)} to={`/home/${props.text.message_group}`}> 
-            <h4 className={style.name}>{props.text.name}</h4> 
-            <span> - </span>
-            <p>{props.text.text}</p>
-            </Link> */}
             <div className={style.name} onClick={()=>setShowProfile(true)}>
-            <img src="https://img.icons8.com/external-others-phat-plus/64/000000/external-classmate-education-and-learning-outline-others-phat-plus-23.png" alt="icona"/>
-            <img src={props.text.photo} alt={props.text.name}/>
-            <h4 to={`/home/${props.text.message_group}`}>{`${props.text.name} ${props.text.lastname}`}</h4> 
+              <img src="https://img.icons8.com/external-others-phat-plus/64/000000/external-classmate-education-and-learning-outline-others-phat-plus-23.png" alt="icona"/>
+              <img src={props.text.photo} alt={props.text.name}/>
+              <h4 to={`/home/${props.text.message_group}`}>{`${props.text.name} ${props.text.lastname}`}</h4> 
             </div>
             
-            {showProfile && <ProfileCard show={showProfile} setShow={setShowProfile} user={props.text}/>}
-            <span> - </span>
-            <Link className= {style.results} onClick={()=> props.setHidden(true)} to={`/home/${props.text.message_group}`}> 
-            
-            <p>{props.text.text}</p>
+            {showProfile && <ProfileCard show={showProfile} setShow={setShowProfile} user={props.text}/>}         
+            <Link className= {style.results} onClick={()=> {props.setHidden(true); props.setInput('')}} to={`/home/${props.text.message_group}`}> 
+              <span> - </span>
+              <p>{props.text.text}</p>
             </Link>
          </div>
   )
--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -6,39 +6,38 @@ import { SearchResults } from "../SearchResults";
 
 export const Searchbar = () => {
   const [input, setInput] = useState("");
-  const groups = useSelector(state => state.groups.map((group)=> group.messages))
+  const groups = useSelector(state => state.groups.map((group) => group.messages))
   const [searchResults, setSearchResult] = useState()
   const users = useSelector(state => state.users)
 
-  const [hidden,setHidden] = useState(false)
-  
+  const [hidden, setHidden] = useState(false)
+
   const search = (e) => {
     setInput(e.target.value);
     setHidden(false)
-    const aa= []
-    groups.map((messages) =>aa.push(...messages))
-    aa.map((message)=> users.map(( user)=> {
-      if(message.author === user.id) {
+    const aa = []
+    groups.map((messages) => aa.push(...messages))
+    aa.map((message) => users.map((user) => {
+      if (message.author === user.id) {
         message.name = user.name
         message.lastname = user.lastname
         message.photo = user.photo
       }
       return message
     }))
-      setSearchResult(aa.filter((message)=> 
-       message.text.toLowerCase().includes(input.toLowerCase())||message.name.toLowerCase().includes(input.toLowerCase()))) 
-    }
+    setSearchResult(aa.filter((message) =>
+      message.text.toLowerCase().includes(input.toLowerCase()) || message.name.toLowerCase().includes(input.toLowerCase())))
+  }
 
- 
   return (
     <div className={style.searchbar}>
       <input type="text" value={input} onChange={search} />
-      <h3 className = {hidden === true  && style.hideResult} onClick={()=> setHidden(true)}>X</h3>
-      {searchResults && <div className={`${style.searched} ${hidden === true  && style.hideResult}`}>
-          
-         {searchResults.map((text, index)=> (
-           <SearchResults setHidden={setHidden} key={index} text={text} user= {text}/>
-         ))}
+      <h3 className={hidden === true && style.hideResult} onClick={() => { setHidden(true); setInput('') }}>X</h3>
+      {searchResults && <div className={`${style.searched} ${hidden === true && style.hideResult}`}>
+
+        {searchResults.map((text, index) => (
+          <SearchResults setHidden={setHidden} setInput={setInput} key={index} text={text} user={text} />
+        ))}
       </div>}
     </div>
   );
