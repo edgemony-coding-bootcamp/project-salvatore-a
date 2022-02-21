bcb2e995b074b0e848f6c815e31fb95dd1dcbeca
Alessandra Perricone <alessandraperricone3@gmail.com>
2022-02-21 12:33:50 +0100

    fixed improvements for Lighthouse

--- src/Components/Message/index.jsx
+++ src/Components/Message/index.jsx
@@ -80,12 +80,12 @@ export const Message = (props) => {
 
 
         <form className={style.updateMessageForm} onSubmit={(e) => handleUpdateMessage(e)}>
-
+        <label className={style.label} for="input">Modifica qui</label>
           <input
             value={updateMessage.message.text}
             onChange={(e) => setUpdateMessage({ ...updateMessage, message: { ...message, text: e.target.value } })}
             className={`${updateMessage.status ? style.updateMessage : style.hiddenUpdateMessage}`}
-            type="text"
+            type="text" id="input"
           />
 
         </form>
