deee21a7020bf6ee2188327e02f800daa589d221
Antonio Tutone <tutoneantonio@gmail.com>
2022-02-22 12:16:55 +0100

    style: modificato stile modali

--- src/Components/MessageBlockModals/index.jsx
+++ src/Components/MessageBlockModals/index.jsx
@@ -1,23 +1,27 @@
 import { Link } from 'react-router-dom';
-import style from '../MessageBlock/MessageBlock.module.scss';
-//import style from "./MessageBlockModals.module.scss";
+import style from "./MessageBlockModals.module.scss";
 
 
 
 
 const MessageBlockModals = (props) => {
-    function handleChangeName(e) {
-        e.preventDefault();
-        props.patchGroups("name", props.group.name, props.updateGroup);
-        props.setModal(false);
-        props.setGroup(props.updateGroup);    
-      }
+  function handleChangeName(e) {
+    e.preventDefault();
+    props.patchGroups("name", props.group.name, props.updateGroup);
+    props.setModal(false);
+    props.setGroup(props.updateGroup);
+  }
 
 
-    return(
-        <>
-        <div className={props.modal ? style.groupModal : null}>
-        <h2>{`#${props.group.name.replace(/_/g," ")}  ` || ""}</h2>
+  return (
+    <div className={style.modalBackground}>
+      <div className={style.groupModal}>
+        <div className={style.head}>
+          <h2>{`#${props.group.name.replace(/_/g, " ")}  ` || ""}</h2>
+          <button className={style.closeButton} onClick={() => props.setModal(false)}>
+            X
+          </button>
+        </div>
         <form onSubmit={(e) => handleChangeName(e)}>
           <label htmlFor="updateGroup">Cambia il Nome del Gruppo:</label>
           <input
@@ -25,29 +29,24 @@ const MessageBlockModals = (props) => {
             onChange={(e) =>
               props.setUpdateGroup({
                 messages: props.group.messages,
-                name: e.target.value.replace(/ /g,"_"),
+                name: e.target.value.replace(/ /g, "_"),
               })
             }
             name="updateGroup"
             type="text"
           />
-          <button onClick={handleChangeName} className={style.modifyButton}><Link to={`/home/${props.updateGroup.name}`}>Modifica</Link></button>
+          <div className={style.buttonGroup}>
+            <button onClick={handleChangeName} className={style.modifyButton}>
+              <Link to={`/home/${props.updateGroup.name}`}>Modifica</Link>
+            </button>
+            <button onClick={() => { props.patchGroups("delete", props.group.name); props.setModal(false) }} className={style.deleteButton}>
+              <Link to={`/home`}>Elimina</Link>
+            </button>
+          </div>
         </form>
-        <div className={style.deleteGroup}>
-          <p>Elimina Gruppo</p>
-          <button onClick={()=> {props.patchGroups("delete",props.group.name);props.setModal(false)} } className={style.deleteButton}>
-          <Link to={`/home`}>Elimina</Link>
-          </button>
-        </div>
-        <button
-          className={style.closeButton}
-          onClick={() => props.setModal(false)}
-        >
-          Chiudi
-        </button>
-      </div>  
-      </>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default MessageBlockModals
--- src/Components/ProfileCard/index.jsx
+++ src/Components/ProfileCard/index.jsx
@@ -8,24 +8,26 @@ const ProfileCard = (props) => {
     const uid = loggedUser.uid;
 
     return (
-        <div className={style.modal}>
-            <div className={style.head}>
-                
-                <button className={style.closeButton} onClick={() => props.setShow(false)}>
-                    X
-                </button>
-            </div>
-            <img src={props.user.photo} alt={props.user.name}></img>
-            <h2 className={style.name}>{props.user.name+ ` ` + props.user.lastname}</h2>
-            <p className={style.email_black}>Email</p>
-            <p className={style.email}>{props.user.email}</p>
-            <Link to="/edit_profile">
-                <button className={ `${props.user.id !== uid ? style.buttonHidden : style.editButton }`}
-                    onClick={() => { props.setShow(false) }}>
-                    Modifica
-                </button>
-            </Link>
+        <div className={style.modal_background}>
+            <div className={style.modal}>
+                <div className={style.head}>
 
+                    <button className={style.closeButton} onClick={() => props.setShow(false)}>
+                        X
+                    </button>
+                </div>
+                <img src={props.user.photo} alt={props.user.name}></img>
+                <h2 className={style.name}>{props.user.name + ` ` + props.user.lastname}</h2>
+                <p className={style.email_black}>Email</p>
+                <p className={style.email}>{props.user.email}</p>
+                <Link to="/edit_profile">
+                    <button className={`${props.user.id !== uid ? style.buttonHidden : style.editButton}`}
+                        onClick={() => { props.setShow(false) }}>
+                        Modifica
+                    </button>
+                </Link>
+
+            </div>
         </div>
     )
 }
