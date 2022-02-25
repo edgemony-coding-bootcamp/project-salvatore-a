8b5b73fb1054c4f0c82015eebd58aebe2cb83261
silvia.hth <silvia.hth@gmail.com>
2022-02-25 18:52:19 +0100

    style:fix

--- src/Components/Sign-up/index.jsx
+++ src/Components/Sign-up/index.jsx
@@ -8,10 +8,28 @@ import style from "./Signup.module.scss";
 
 const Signup = (props) => {
     const [user,setUser] = useState({})
+    const [banner, setBanner]= useState({ display: false})
 
     const handleSignup = (e)=>{
         e.preventDefault();
-        signup(user)
+        if(e.target[3].value.length>5 ) {
+            signup(user);
+        setBanner({
+            message: "Registrazione avvenuta con successo!",
+            display: true})
+        setTimeout(() => {
+            setBanner({display:false})
+            handleComeBack()
+        }, 2000);
+        }
+       else{
+        setBanner({
+            message: "La password deve contenere almeno 6 caratteri",
+            display: true})
+        setTimeout(() => {
+            setBanner({display:false})
+        }, 2000);
+       }
     }
     function handleComeBack(){
         props.setter(false)
@@ -19,24 +37,19 @@ const Signup = (props) => {
     return(
         
         <div className={style.wrapper}>
+        {banner.display && <p className={style.banner}>{banner.message}</p>}
         <div className={style.wrapper_reg}>
                 <form onSubmit={handleSignup}>
                     
                     <div className={style.wrapper_item}>
                         <label htmlFor="name"> Nome </label>
                         <input onChange={(e)=> setUser({...user,name:e.target.value})} type="text" placeholder="name" id="name" required />
-                    
-
-                    
+   
                         <label htmlFor="surname"> Cognome </label>
                         <input onChange={(e)=> setUser({...user,lastname:e.target.value})} type="text" placeholder="lastname" id="surname" required />
-                    
-                    
-                    
+        
                         <label htmlFor="email"> E-mail </label>
                         <input onChange={(e)=> setUser({...user,email:e.target.value})} type="email" placeholder="email" id="email" required />
-                    
-            
                     
                         <label htmlFor="password"> Password </label>
                         <input onChange={(e)=> setUser({...user,password:e.target.value})} type="password" placeholder="password" id="password" required/>
@@ -45,7 +58,7 @@ const Signup = (props) => {
                     
                     <button>Registrati</button>
                 </form>
-                <p onClick={()=> handleComeBack()}>Torna al Login </p>
+                <p onClick={handleComeBack}>Torna al Login </p>
                 </div>
             </div>
         
