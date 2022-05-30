9d12d056b9aec454a0ec5cff32194cca02096e46
Alessia Rappa <alessia.rappa_94@libero.it>
2022-05-30 19:06:35 +0200

    feat: footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
