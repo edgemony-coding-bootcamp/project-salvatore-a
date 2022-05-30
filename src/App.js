ec1e311800a187aad486d9e8ae63f95e79ce6f82
carmelofileccia <carmelofileccia@gmail.com>
2022-05-30 18:00:59 +0200

    feat: hero-with-video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Hero />
     </div>
   );
 }
