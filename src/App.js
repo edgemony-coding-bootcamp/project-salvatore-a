1949dfe74c8b4b4a1fc70cc4152bb1d7caeb7a93
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-05-31 13:33:01 +0200

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
-import UserContextProvider from "./Context/UserContext/UserProvider";
-import MovieContextProvider from "./Context/MovieContext/MovieProvider";
 import SliderWrapper from "./components/SliderWrapper";
-import ModalPlay from './components/ModalPlay/ModalPlay';
+import ModalPlay from "./components/ModalPlay/ModalPlay";
 
 import UserContextProvider from "./Context/UserContext/UserProvider";
 import MovieContextProvider from "./Context/MovieContext/MovieProvider";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [isVisible, setVisible] = useState(false)
+  const [isVisible, setVisible] = useState(false);
 
   const toggleModal = () => {
-    setVisible(!isVisible)
-  }
+    setVisible(!isVisible);
+  };
   return (
-
     <div className={styles.App}>
       <UserContextProvider>
         <Header />
