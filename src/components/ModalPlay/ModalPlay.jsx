a2eb493f1522af1a644959de3a0db5b410facbea
carmelofileccia <carmelofileccia@gmail.com>
2022-05-31 13:18:59 +0200

    feat/modalPlay

---
+++ src/components/ModalPlay/ModalPlay.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import { IoIosCloseCircleOutline } from 'react-icons/io'
+import styles from "./ModalPlay.module.scss";
+import stranger from "./../../../src/logo.png"
+
+
+export default function ModalPlay({ isVisible, toggleModal }) {
+
+
+
+    return (
+        <div >
+            {isVisible && (
+                <>
+                    <div className={styles.Overlay}> </div>
+                    <div className={styles.ModalContainer}>
+                        <img src={stranger} alt={'Edgeflix'} />
+                        <h2 className={styles.ModalContainer__Title}>Al momento non è possibile  riprodurre questo titolo.Riprova più tardi, quando verremo assunti da Netflix </h2>
+                        <h3 className={styles.ModalContainer__TitleTwo}>p.s. Se potete, metteteci una buona parola</h3>
+                        <button onClick={() => toggleModal()} className={styles.ModalContainer__BtnClose}>  Torna a Edgeflix</button>
+
+                    </div>
+                </>
+
+            )}
+        </div>
+    );
+}
