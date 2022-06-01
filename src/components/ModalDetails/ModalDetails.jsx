009fccdcfe2ee7e9279ba743125770a5e4cda1cf
carmelofileccia <101353110+carmelofileccia@users.noreply.github.com>
2022-06-01 18:37:33 +0200

    Feat/rating (#168)
    * add Rating
    
    * fix rating render

--- src/components/ModalDetails/ModalDetails.jsx
+++ src/components/ModalDetails/ModalDetails.jsx
@@ -1,4 +1,6 @@
 import styles from "./ModalDetails.module.scss";
+import Rating from "./../Rating/Rating";
+
 import { useState } from 'react';
 import { IoIosCloseCircleOutline } from 'react-icons/io';
 import { MdAddCircleOutline, MdPlayCircleOutline } from 'react-icons/md'
@@ -20,10 +22,11 @@ export default function ModalDetails({ isVisible, movieData, toggleModal, toggle
                         <img className={styles.ModalContainer__Poster} src={movieData.poster} alt={movieData.title} />
 
                         <div className={styles.ModalData}>
+                            <Rating movieData={movieData} setRender={setRender} />
                             <h1 className={styles.ModalData__Title}>{movieData.title}</h1>
                             <h3 className={styles.ModalData__Season}> Stagioni: {movieData.seasons} </h3>
                             <h3 className={styles.ModalData__Desc}>{movieData.description}</h3>
-                            <h4 className={styles.ModalData__Genres}> Genere: {movieData.genres.join(", ")}</h4>
+                            <h4 className={styles.ModalData__Cast}> Genere: {movieData.genres.join(", ")}</h4>
                             <h4 className={styles.ModalData__Cast}> Cast:{movieData.cast.join(", ")}</h4>
                             {isFavorite ? (<FiMinusCircle className={styles.ModalData__BtnCir}
                                 onClick={() => {
@@ -36,6 +39,7 @@ export default function ModalDetails({ isVisible, movieData, toggleModal, toggle
                                 }}
                                     className={styles.ModalData__BtnCir} />}
                             <MdPlayCircleOutline onClick={() => togglePlayModal()} className={styles.ModalData__BtnCir} />
+
                         </div>
                     </div>
                 </>
---
+++ src/components/Rating/Rating.jsx
@@ -0,0 +1,46 @@
+import styles from "./Rating.module.scss";
+import { useMovieContext } from './../../Context/MovieContext/MovieProvider';
+import { AiFillStar } from 'react-icons/ai'
+import { useState, useEffect } from 'react';
+
+
+export default function Rating({ movieData, setRender }) {
+    const [rating, setRating] = useState(0);
+    const addRating = useMovieContext().movieRating
+
+    useEffect(() => {
+        if (movieData) {
+            setRating(movieData.rating);
+            (console.log(movieData.rating))
+        }
+
+        //eslint-disable-next-line
+    }, [movieData]);
+
+    return (
+        <>
+            {movieData &&
+                <div className={styles.Rating}>
+                    {[...Array(5)].map((star, index) => {
+                        index += 1;
+                        return (
+                            <button
+                                type="button"
+                                key={index}
+                                className={index <= rating ? styles.Rating__On : styles.Rating__Off}
+                                onClick={() => {
+                                    movieData && addRating(movieData.id, index)
+                                        .then(() => { setRender(prev => !prev); setRating(index) })
+                                }}
+                            >
+                                {/* {console.log(movieData.id)} */}
+                                {/* {console.log(movieData)} */}
+                                <span className={styles.Rating__Span}><AiFillStar className={styles.Rating__Star} /></span>
+                            </button>
+                        );
+                    })}
+                </div>
+            }
+        </>
+    );
+};
