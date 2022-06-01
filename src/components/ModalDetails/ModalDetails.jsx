82af2c3134babf8fc1165ca1b935d5a53bb9faaf
carmelofileccia <carmelofileccia@gmail.com>
2022-06-01 11:09:27 +0200

    fix: join genres-cast/add onclick overlay

--- src/components/ModalDetails/ModalDetails.jsx
+++ src/components/ModalDetails/ModalDetails.jsx
@@ -9,9 +9,7 @@ export default function ModalDetails({ isVisible, movieData, toggleModal, toggle
         <div >
             {isVisible && (
                 <>
-                    {console.log(movieData)}
-
-                    <div className={styles.Overlay}> </div>
+                    <div onClick={() => toggleModal()} className={styles.Overlay}> </div>
                     <div className={styles.ModalContainer}>
                         <IoIosCloseCircleOutline onClick={() => toggleModal()} className={styles.ModalContainer__BtnClose} />
 
@@ -22,8 +20,8 @@ export default function ModalDetails({ isVisible, movieData, toggleModal, toggle
                             <h1 className={styles.ModalData__Title}>{movieData.title}</h1>
                             <h3 className={styles.ModalData__Season}> Stagioni: {movieData.seasons} </h3>
                             <h3 className={styles.ModalData__Desc}>{movieData.description}</h3>
-                            <h4 className={styles.ModalData__Genres}> Genere: {movieData.genres}</h4>
-                            <h4 className={styles.ModalData__Cast}> Cast:{movieData.cast}</h4>
+                            <h4 className={styles.ModalData__Genres}> Genere: {movieData.genres.join(", ")}</h4>
+                            <h4 className={styles.ModalData__Cast}> Cast:{movieData.cast.join(", ")}</h4>
                             <MdAddCircleOutline onClick={() => { }} className={styles.ModalData__BtnCir} />
                             <MdPlayCircleOutline onClick={() => togglePlayModal()} className={styles.ModalData__BtnCir} />
                         </div>
--- src/components/ModalPlay/ModalPlay.jsx
+++ src/components/ModalPlay/ModalPlay.jsx
@@ -6,7 +6,7 @@ export default function ModalPlay({ isVisible, toggleModal }) {
         <div >
             {isVisible && (
                 <>
-                    <div className={styles.Overlay}> </div>
+                    <div onClick={() => toggleModal()} className={styles.Overlay}> </div>
                     <div className={styles.ModalContainer}>
                         <img src={stranger} alt={'Edgeflix'} />
                         <h2 className={styles.ModalContainer__Title}>Al momento non è possibile  riprodurre questo titolo.Riprova più tardi, quando verremo assunti da Netflix </h2>
