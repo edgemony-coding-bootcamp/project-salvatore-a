454e0a3cc0d26a243eab0bb30c2a6e34702d6c9e
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-06-05 19:48:54 +0200

    fix: user redirect (#210)

--- src/components/Protected/Protected.jsx
+++ src/components/Protected/Protected.jsx
@@ -1,7 +1,22 @@
 import Homepage from "../../pages/Homepage";
 import Signin from "../../pages/Signin";
+import Signup from "../../pages/Signup";
 
-export default function Protected() {
+export default function Protected({ redirect }) {
   const isUserLogged = localStorage.getItem("currentUser") !== null;
-  return <>{isUserLogged ? <Homepage /> : <Signin />}</>;
+  return (
+    <>
+      {redirect === "Signin" ? (
+        isUserLogged ? (
+          <Homepage />
+        ) : (
+          <Signin />
+        )
+      ) : isUserLogged ? (
+        <Homepage />
+      ) : (
+        <Signup />
+      )}
+    </>
+  );
 }
