44097d8a2ad337472191160922dc36972aa6a5d4
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-06-05 17:26:00 +0200

    fix: userMenu bugs on mobile

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,7 +1,7 @@
 import styles from "./Overlay.module.scss"
 
-export default function Overlay ({functionOnClick = ()=>{}}) {
+export default function Overlay ({functionOnClick = ()=>{}, visibility=true}) {
     return (
-        <div onClick={()=>functionOnClick()} className={styles.Overlay}></div>
+        <div onClick={()=>functionOnClick()} className={visibility ? styles.Overlay : styles.OverlayNotVisible}></div>
     )
 }
--- src/components/UsersMenu/UsersMenu.jsx
+++ src/components/UsersMenu/UsersMenu.jsx
@@ -1,10 +1,17 @@
 import { MdArrowDropDown } from "react-icons/md";
 import { BsPencil } from "react-icons/bs";
 import { AiOutlineUser, AiOutlineQuestionCircle } from "react-icons/ai";
+
 import { useEffect, useState } from "react";
 import { useUserContext } from "../../Context/UserContext/UserProvider";
-import styles from "./UsersMenu.module.scss";
 import { useNavigate } from "react-router-dom";
+
+import adminIcon from "./../../assets/adminIcon.png";
+import anonymousUser from "./../../assets/anonymousUser.png";
+
+import Overlay from "./../Overlay";
+
+import styles from "./UsersMenu.module.scss";
 
 export default function UsersMenu() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -31,68 +38,83 @@ export default function UsersMenu() {
   };
 
   return (
-    <div
-      className={styles.UsersMenu}
-      onMouseOver={() => {
-        setMenuOpen(true);
-      }}
-      onClick={() => setMenuOpen((prev) => !prev)}
-    >
-      {actualUser && (
-        <img
-          alt={actualUser.username || "Anonimo"}
-          src={
-            actualUser.avatar ||
-            "https://cdn-icons-png.flaticon.com/512/843/843331.png?w=740"
-          }
-        ></img>
-      )}
-      <MdArrowDropDown />
+    <>
       {isMenuOpen && (
-        <div
-          className={styles.UsersMenu__DropdownMenu}
-          onMouseLeave={() => {
-            return isMenuOpen
-              ? setTimeout(() => setMenuOpen(false), 500)
-              : null;
-          }}
-        >
-          <ul>
-            {actualUser && (
-              <li key={actualUser?.id}>
-                <img
-                  className={styles.UsersMenu__DropdownMenu__UserIcon}
-                  src={
-                    actualUser?.avatar ||
-                    "https://cdn-icons-png.flaticon.com/512/843/843331.png?w=740"
-                  }
-                  alt={actualUser?.username || "Anonimo"}
-                />
-                {actualUser?.username || "Anonimo"}
-              </li>
-            )}
-
-            <li>
-              <BsPencil /> Gestisci il profilo
-            </li>
-            <hr />
-            <li>
-              <AiOutlineUser /> Account
-            </li>
-            <li>
-              <AiOutlineQuestionCircle />
-              Centro assistenza
-            </li>
-            <hr />
-            <li
-              onClick={() => logout()}
-              className={styles.UsersMenu__DropdownMenu__Exit}
-            >
-              Esci da Edgeflix
-            </li>
-          </ul>
-        </div>
+        <Overlay
+          visibility={false}
+          functionOnClick={() => setMenuOpen(false)}
+        />
       )}
-    </div>
+      <div
+        className={styles.UsersMenu}
+        onMouseOver={() => (window.innerWidth > 700 ? setMenuOpen(true) : {})}
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        {actualUserID === "admin" ? (
+          <img alt={"Admin Icon"} src={adminIcon}></img>
+        ) : (
+          actualUser && (
+            <img
+              alt={actualUser?.username || "Anonimo"}
+              src={actualUser?.avatar || anonymousUser}
+            ></img>
+          )
+        )}
+        <MdArrowDropDown />
+        {isMenuOpen && (
+          <div
+            className={styles.UsersMenu__DropdownMenu}
+            onMouseLeave={() => {
+              return isMenuOpen
+                ? setTimeout(() => setMenuOpen(false), 500)
+                : null;
+            }}
+          >
+            <ul>
+              {actualUserID === "admin" ? (
+                <li>
+                  <img
+                    className={styles.UsersMenu__DropdownMenu__UserIcon}
+                    src={adminIcon}
+                    alt={"Admin Icon"}
+                  />
+                  {"Admin"}
+                </li>
+              ) : (
+                actualUser && (
+                  <li key={actualUser?.id}>
+                    <img
+                      className={styles.UsersMenu__DropdownMenu__UserIcon}
+                      src={actualUser?.avatar || anonymousUser}
+                      alt={actualUser?.username || "Anonimo"}
+                    />
+                    {actualUser?.username || "Anonimo"}
+                  </li>
+                )
+              )}
+
+              <li>
+                <BsPencil /> Gestisci il profilo
+              </li>
+              <hr />
+              <li>
+                <AiOutlineUser /> Account
+              </li>
+              <li>
+                <AiOutlineQuestionCircle />
+                Centro assistenza
+              </li>
+              <hr />
+              <li
+                onClick={() => logout()}
+                className={styles.UsersMenu__DropdownMenu__Exit}
+              >
+                Esci da Edgeflix
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
